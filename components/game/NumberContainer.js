a7adfefa52badb8b75d98f75dd612d5d2cffc4a4
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-03-11 18:06:25 +0200

    'all'

--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -1,30 +1,33 @@
-import { StyleSheet, Text, View } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
 
+import Colors from '../../constants/colors';
 
-const NumberContainer = ({ children }) => {
-
-    return (
-        <View style={styles.numberContainer}>
-            <Text style={styles.numberText} >{children}</Text>
-        </View>
-    );
-};
+function NumberContainer({ children }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.numberText}>{children}</Text>
+    </View>
+  );
+}
 
 export default NumberContainer;
 
+const deviceWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
-    numberContainer: {
-        borderWidth: 4,
-        borderColor: '#ddb52f',
-        padding: 30,
-        margin: 20,
-        borderRadius: 8,
-    },
-    numberText: {
-        color: '#ddb52f',
-        fontSize: 32,
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
+  container: {
+    borderWidth: 4,
+    borderColor: Colors.accent500,
+    padding: deviceWidth < 380 ? 12 : 24,
+    margin: deviceWidth < 380 ? 12 : 24,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  numberText: {
+    color: Colors.accent500,
+    fontSize: deviceWidth < 380 ? 24 : 36,
+    // fontWeight: 'bold',
+    fontFamily: 'open-sans-bold'
+  },
 });
--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,21 +1,28 @@
-const { View, StyleSheet } = require("react-native")
+import { View, StyleSheet, Dimensions } from 'react-native';
 
-const Card = ({ children }) => {
+import Colors from '../../constants/colors';
 
-    return (
-        <View style={styles.inputContainer} >{children}</View>
-    );
-};
+function Card({ children }) {
+  return <View style={styles.card}>{children}</View>;
+}
 
 export default Card;
 
+const deviceWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
-    inputContainer: {
-        backgroundColor: '#6F1E51',
-        padding: 20,
-        marginTop: 100,
-        marginHorizontal: 30,
-        borderRadius: 8,
-        alignItems: 'center',
-    },
+  card: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: deviceWidth < 380 ? 18 : 36,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: Colors.primary800,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
 });
